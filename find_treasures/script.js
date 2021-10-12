9a7de5c786974c30131a17a0a484a4d8d9b08a40
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2021-10-12 23:43:03 +0200

    Add getRandomNumber function

---
+++ find_treasures/script.js
@@ -0,0 +1,6 @@
+// Generate random number from 0 to sizeOfMap-1 to set the position of the treasures
+var getRandomNumber = function (sizeOfMap) {
+  return Math.floor(Math.random() * sizeOfMap);
+};
+
+console.log(Math.random());
